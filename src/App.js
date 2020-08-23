4480edb1cafbd343eb4a638cf7cc5e18ea52ebee
German Nunez <german90nunez@gmail.com>
2020-08-22 17:16:29 -0700

    cleaned house and gathered countries data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    axios.get(`https://restcountries.eu/rest/v2/all`)
+      .then(res => {
+        setCountries(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Filter Countries</h1>
     </div>
   );
 }
